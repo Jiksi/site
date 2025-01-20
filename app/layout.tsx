c6deede1ffcc45c1ac7e72c0d646b87d317bbb3c
Zhicxi Azis Pramana <zhicxiap99@gmail.com>
2025-01-20 11:42:45 +0800

    Update layout to use min-h-svh for improved responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
               <feColorMatrix type="saturate" values="0" />
             </filter>
           </svg>
-          <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 bg-white text-gray-900">
+          <div className="min-h-svh flex flex-col justify-between pt-0 md:pt-8 p-8 bg-white text-gray-900">
             <main className="max-w-[60ch] mx-auto w-full space-y-6">
               {children}
             </main>
