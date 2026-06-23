da07db71866a8ae8894af70a8861ca19cc7abd64
jiksi <zhicxiap99@gmail.com>
2026-06-23 18:02:15 +0800

    fix: adjust margin for AnimatedName link for better spacing

--- app/work/animated-name.tsx
+++ app/work/animated-name.tsx
@@ -2,7 +2,7 @@ import { Link } from "next-view-transitions";
 
 export function AnimatedName() {
   return (
-    <Link href="/" className="flex mb-8 font-medium text-gray-400 fade-in">
+    <Link href="/" className="flex mb-4 font-medium text-gray-400 fade-in">
       Zhicxi Azis Pramana
     </Link>
   );
