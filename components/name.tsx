77577fbbadaa6f04cf6ffab55fe3bf49cb6de791
jiksi <zhicxiap99@gmail.com>
2026-06-25 20:34:44 +0800

    fix: add w-fit class to improve layout consistency for AnimatedName and NameTransition components

--- components/name.tsx
+++ components/name.tsx
@@ -1,6 +1,6 @@
 export function NameTransition() {
   return (
-    <h1 className="font-medium transition-element">
+    <h1 className="font-medium transition-element w-fit">
       <span className="sr-only">Zhicxi Azis Pramana</span>
       <span aria-hidden="true" className="block overflow-hidden group relative">
         <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full whitespace-nowrap">
